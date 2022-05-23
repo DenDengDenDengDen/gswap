50bd08a8a91e5ded497807d2216cb0b4c63be83e
黄永煊 <yxhuang@szzhice.com>
2022-05-23 17:50:48 +0800

    1

---
+++ src/constants/multicall/index.ts
@@ -0,0 +1,13 @@
+import { ChainId } from 'hyxgsswapsdkhahaheihei'
+import MULTICALL_ABI from './abi.json'
+
+const MULTICALL_NETWORKS: { [chainId in ChainId]: string } = {
+  [ChainId.MAINNET]: '0xeefBa1e63905eF1D7ACbA5a8513c70307C1cE441',
+  [ChainId.ROPSTEN]: '0x53C43764255c17BD724F74c4eF150724AC50a3ed',
+  [ChainId.KOVAN]: '0x2cc8688C5f75E365aaEEb4ea8D6a480405A48D2A',
+  [ChainId.RINKEBY]: '0x42Ad527de7d4e9d9d011aC45B31D8551f8Fe9821',
+  [ChainId.GÖRLI]: '0x77dCa2C955b15e9dE4dbBCf1246B4B85b651e50e',
+  [ChainId.GS] : '0xe20EF4F8b0A7308ed2208583F1B72F05631bb61F',
+}
+
+export { MULTICALL_ABI, MULTICALL_NETWORKS }
